9c1d7c6910c5e7602cfb85eaafc1b081f60d4d53
cpesolariummc <cpe.solares.johndaril.c@gmail.com>
2026-05-20 17:13:57 +0800

    Enhance game flow: increase startup delay, add user ID to registration, and improve login handling

--- main.cpp
+++ main.cpp
@@ -21,16 +21,14 @@ using namespace std;
 
 bool login(const string& username, const string& password) {
     ifstream infile("userdata.txt");
-    string fileusername, filepassword;
-
-   while (infile >> fileusername >> filepassword) {
+    string fileusername, filepassword, fileId;
+    while (infile >> fileusername >> filepassword >> fileId) {
         if (fileusername == username && filepassword == password) {
             return true;
         }
     }
     return false;
 }
-
 
 void registerUser(PlayerData& player) {
     int id = rand() % 100000; 
@@ -39,78 +37,87 @@ void registerUser(PlayerData& player) {
 }
 int main() {
     srand(static_cast<unsigned int>(time(0)));
+    bool running=true;
+    while (running){
+        string username, password;
+        int choice;
+        outline();
+        cout << "Welcome to the Flower Game!" << endl;
+        cout << "1. Register" << endl;
+        cout << "2. Login" << endl;
+        outline();
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    string username, password;
-    int choice;
-    outline();
-    cout << "Welcome to the Flower Game!" << endl;
-    cout << "1. Register" << endl;
-    cout << "2. Login" << endl;
-    outline();
-    cout << "Enter your choice: ";
-    cin >> choice;
+        if (choice == 1) {
+            string newUsername, newPassword;
+            cout << "Enter username: ";
+            cin >> newUsername;
+            cout << "Enter password: ";
+            cin >> newPassword;
+            ifstream infile("userdata.txt");
+            string fileusername, filepassword;
+            bool userExists = false;
+            while (infile >> fileusername >> filepassword) {
+                if (fileusername == newUsername) {
+                    userExists = true;
+                    break;
+                }
+            }
+            if (userExists) {
+                cout << "Username already exists. Please choose a different username." << endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(300));
+                
+            } else {
+                int id = rand() % 100000;
+                ofstream outfile("userdata.txt", ios::app);
+                outfile << newUsername << " " << newPassword << " " << id << " " << endl;
+                cout << "Registration successful!" << endl;
+            }
+            
+        
+        } else if (choice == 2) {
+            cout << "Enter username: ";
+            cin >> username;
+            cout << "Enter password: ";
+            cin >> password;
 
-    if (choice == 1) {
-        string newUsername, newPassword;
-        cout << "Enter username: ";
-        cin >> newUsername;
-        cout << "Enter password: ";
-        cin >> newPassword;
-        ifstream infile("userdata.txt");
-        string fileusername, filepassword;
-        bool userExists = false;
-        while (infile >> fileusername >> filepassword) {
-            if (fileusername == newUsername) {
-                userExists = true;
-                break;
+        if(login(username, password)) 
+            {
+                outline();
+                cout << "Login successful! Welcome, " << username << "!" << endl;
+                outline();
+                std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+                cout << "Welcome to the Flower Game!" << endl;  
+                
+                std::this_thread::sleep_for(std::chrono::milliseconds(300));
+                outline();
+                cout << "1. Start Game" << endl;   
+                cout << "2. Logout" << endl;
+                outline();
+                cout << "Enter your choice: ";
+                cin >> choice;
+                if (choice == 1) {
+                    Start();
+                }else{
+                    cout << "Logging out.."<<endl;
+                    main();
+                };
+            }
+            else {
+                cout << "Invalid username or password." << endl;
+                outline();
+                std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+                
             }
         }
-        if (userExists) {
-            cout << "Username already exists. Please choose a different username." << endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(300));
-            
-        } else {
-            int id = rand() % 100000;
-            ofstream outfile("userdata.txt", ios::app);
-            outfile << newUsername << " " << newPassword << " " << id << " " << endl;
-            cout << "Registration successful!" << endl;
-        }
-        
-    
-    } else if (choice == 2) {
-        cout << "Enter username: ";
-        cin >> username;
-        cout << "Enter password: ";
-        cin >> password;
-
-        while (login(username, password)) 
-        {
+        else {
             outline();
-            cout << "Login successful! Welcome, " << username << "!" << endl;
+            cout << "Invalid choice. Please try again." << endl;
             outline();
-            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-            cout << "Welcome to the Flower Game!" << endl;  
-            
-            std::this_thread::sleep_for(std::chrono::milliseconds(300));
-            outline();
-            cout << "1. Start Game" << endl;   
-            cout << "2. Logout" << endl;
-            outline();
-            cout << "Enter your choice: ";
-            cin >> choice;
-            if (choice == 1) {
-                Start();
-            }else{
-                cout << "Logging out.."<<endl;
-                main();
-            };
+            main();
         }
     }
-    else {
-        outline();
-        cout << "Invalid choice. Please try again." << endl;
-        outline();
-        main();
-    }
+return 0;
 }
 #endif//MAIN_CPP
